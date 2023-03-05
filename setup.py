f9c9c61a701a7504f56f2adb5f478febe4360287
backwardspy <backwardspy@pigeon.life>
2023-03-05 22:33:40 +0000

    chore: update author email

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup
 
-
 setup(
     name="gunicorn-healthz-filter",
     version="1.0",
     description="Filter /healthz endpoint access from your gunicorn logs.",
     author="backwardspy",
-    author_email="backwardspy@gmail.com",
+    author_email="backwardspy@pigeon.life",
     packages=["gunicorn_healthz_filter"],
     install_requires=["gunicorn"],
 )
